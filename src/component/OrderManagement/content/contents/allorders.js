cf119a5e99a4379d9e5a5f51074a388bc82b7848
xy <3311599023@qq.com>
2018-01-08 14:56:48 +0800

    弹出窗口父传子组件状态，点击close关闭更改状态并传到父组件。状态还原。

--- src/component/OrderManagement/content/contents/allorders.js
+++ src/component/OrderManagement/content/contents/allorders.js
@@ -1,23 +1,27 @@
 import React,{Component} from 'react';
 import conts from  '../contents.css';
 import Pops from '../popups/popups';
+
 export default class AllOrders extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {keys:1}
 	}
-  ClickShow=()=>{
-
+  ClickShow=(props)=>{
+      this.setState({
+        keys: this.state.keys+1
+      })
   }
-
 	render(){
+	  let keys = this.state;
+	  console.log(keys);
 		return (
 			<div className={conts.pop}>
 				<div className={conts.letEf}>
 					全部订单出现了
-          <button>点击我</button>
+          <button onClick={this.ClickShow}>点击我</button>
 				</div>
-				<Pops />
+				 <Pops keys={this.props.keys} />{keys>=1 ?2:null}
 			</div>
 		)
 	}
--- src/component/OrderManagement/content/popups/popups.js
+++ src/component/OrderManagement/content/popups/popups.js
@@ -2,32 +2,21 @@ import React, {Component} from 'react';
 import pop from './popups.css';
 /*
 *
-* 父传子来显示   有新订单
-* 点击close后，如果无数据状态就一直隐藏，如果状态更新就显示
+* 父传子来显示订单
+* 点击close
 *
 * */
 class Pops extends Component {
   constructor (props) {
     super(props);
-    this.state = {display: 'showBottom',sates:'0'};
-    this.closes = this.closes.bind(this);
-  }
-  closes(){
-   this.setState( prevState =>({
-     display: !prevState.display,
-     states: !prevState.states
-     })
-
-   )
   }
   render (){
-    let act = this.state.display ? 'showBottom' : 'hide';
-    let keys = this.state.states ? '0':'1';
-    console.log(keys);
+    let act = this.props.keys;
+    console.log(act+"a111111");
     return (
-      <div className={act} key={keys}>
+      <div className={act}>
         <div style={{position:'relative',height:'40px',width:'100%'}} >
-          <i className={pop.close} onClick={this.closes}>X</i>
+          <i className={pop.close} >X</i>
         </div>
         <div style={{lineHeight:'100px'}}>您有新的订单</div>
       </div>
